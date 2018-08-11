a83aca06b583fe72bde1fdd684c7837c5f2ae348
ytakahashi <yt01tc@gmail.com>
2018-08-11 10:33:33 +0900

    mega evolution

--- src/name.js
+++ src/name.js
@@ -5,7 +5,8 @@ const jsons = [].concat(
   require('./resources/pokemon/gen4_name.json'),
   require('./resources/pokemon/gen5_name.json'),
   require('./resources/pokemon/gen6_name.json'),
-  require('./resources/pokemon/gen7_name.json')
+  require('./resources/pokemon/gen7_name.json'),
+  require('./resources/pokemon/mega_name.json')
 )
 
 const nameMap = new Map(jsons.map(obj => [obj.id, obj.name]))
--- test/test_pokemon.js
+++ test/test_pokemon.js
@@ -5,7 +5,7 @@ const expect = require('chai').expect
 const { Pokemon } = require('../src/pokemon')
 
 describe('Pokemon class (language: ja)', () => {
-  it('returns corrent properties', () => {
+  it('returns corrent properties for ピカチュウ', () => {
     const pikachu = new Pokemon({
       'id': '25',
       'name': 'ピカチュウ',
@@ -49,5 +49,123 @@ describe('Pokemon class (language: ja)', () => {
       .that.has.lengthOf(2)
       .and.that.include('陸上')
       .and.that.include('妖精')
+    expect(pikachu.megaEvolution)
+      .to.be.undefined
+  })
+
+  it('returns corrent properties for リザードン', () => {
+    const actual = new Pokemon({
+      'id': '6',
+      'name': 'リザードン',
+      'type': [
+        'ほのお',
+        'ひこう'
+      ],
+      'abilities': [
+        {
+          'name': 'もうか'
+        },
+        {
+          'name': '*サンパワー'
+        }
+      ],
+      'status': {
+        'H': '78',
+        'A': '84',
+        'B': '78',
+        'C': '109',
+        'D': '85',
+        'S': '100'
+      },
+      'egg_groups': [
+        '怪獣',
+        'ドラゴン'
+      ],
+      'mega_evolve': true
+    },
+    1,
+    'ja')
+
+    expect(actual.id)
+      .to.equal('6')
+    expect(actual.name)
+      .to.equal('リザードン')
+    expect(actual.type)
+      .to.be.an('array')
+      .that.has.lengthOf(2)
+      .and.that.include('ほのお')
+      .and.that.include('ひこう')
+    expect(actual.baseStats)
+      .to.have.all.keys('H', 'A', 'B', 'C', 'D', 'S')
+    expect(actual.eggGroup)
+      .to.be.an('array')
+      .that.has.lengthOf(2)
+      .and.that.include('怪獣')
+      .and.that.include('ドラゴン')
+    expect(actual.megaEvolution)
+      .to.be.an('array')
+      .that.has.lengthOf(2)
+    expect(actual.megaEvolution[0])
+      .to.have.all.keys('name', 'ability', 'type', 'baseStats')
+    expect(actual.megaEvolution[1])
+      .to.have.all.keys('name', 'ability', 'type', 'baseStats')
+  })
+})
+
+describe('Pokemon class (language: en)', () => {
+  it('returns corrent properties for Venusaur', () => {
+    const actual = new Pokemon({
+      'id': '3',
+      'name': 'フシギバナ',
+      'type': [
+        'くさ',
+        'どく'
+      ],
+      'abilities': [
+        {
+          'name': 'しんりょく'
+        },
+        {
+          'name': '*ようりょくそ'
+        }
+      ],
+      'status': {
+        'H': '80',
+        'A': '82',
+        'B': '83',
+        'C': '100',
+        'D': '100',
+        'S': '80'
+      },
+      'egg_groups': [
+        '怪獣',
+        '植物'
+      ],
+      'mega_evolve': true
+    },
+    1,
+    'en')
+
+    expect(actual.id)
+      .to.equal('3')
+    expect(actual.name)
+      .to.equal('Venusaur')
+    expect(actual.type)
+      .to.be.an('array')
+      .that.has.lengthOf(2)
+      .and.that.include('Grass')
+      .and.that.include('Poison')
+    expect(actual.baseStats)
+      .to.have.all.keys('H', 'A', 'B', 'C', 'D', 'S')
+    expect(actual.eggGroup)
+      .to.be.an('array')
+      .that.has.lengthOf(2)
+      .and.that.include('Monster')
+      .and.that.include('Grass')
+    expect(actual.megaEvolution)
+      .to.be.an('array')
+      .that.has.lengthOf(1)
+    expect(actual.megaEvolution[0])
+      .to.have.all.keys('name', 'ability', 'type', 'baseStats')
   })
 })
